6516aa9221d06e8b3bad67f76c4511a29615a6d7
Juan Gabriel Espinoza <50055314+espinozajuan@users.noreply.github.com>
2022-01-18 15:58:31 -0300

    thumbnails clickable, fixed prev & next icons in carousel

--- src/components/Product/ProductImages.js
+++ src/components/Product/ProductImages.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './ProductImages.module.css';
 
 const ProductImages = props => {
+  const [mainProductImgSrc, setMainProductImgSrc] = useState(`../../images/${props.images[0]}`);
+
+  const mainProductImgSrcHandler = event => {
+    setMainProductImgSrc(event.target.getAttribute('src').replace('-thumbnail', ''));
+  }
+
   return (
     <div className={styles.productImagesWrapper}>
       <div className={styles.mainProductImageWrapper}>
-        <img onClick={props.onShowCarousel} className={styles.mainProductImage} src={`../../images/${props.images[0]}`} alt={props.images[0].split('.')[0]}/>
+        <img onClick={props.onShowCarousel} className={styles.mainProductImage} src={mainProductImgSrc} alt={mainProductImgSrc}/>
       </div>
       <div className={styles.productThumbsWrapper}>
         {props.images.map((thumb, idx) => (
-          <div className={[styles.productThumbWrapper, !idx ? styles.active : ''].join(' ')} key={idx}>
-            <img className={styles.productThumb} src={`../../images/${thumb.split('.')[0]}-thumbnail.jpg`} alt={`${thumb.split('.')[0]}-thumbnail`}/>
+          <div className={[styles.productThumbWrapper, mainProductImgSrc.replace('../../images/', '') == thumb ? styles.active : ''].join(' ')} key={idx}>
+            <img onClick={mainProductImgSrcHandler} className={styles.productThumb} src={`../../images/${thumb.split('.')[0]}-thumbnail.jpg`} alt={`${thumb.split('.')[0]}-thumbnail`}/>
           </div>
         ))}
       </div>
