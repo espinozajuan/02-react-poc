e284f202ca09a3b31e83e8c00977d6b2b9ff8e91
Juan Gabriel Espinoza <50055314+espinozajuan@users.noreply.github.com>
2022-01-18 16:48:25 -0300

    responsive fix

--- src/components/Product/ProductInfo.js
+++ src/components/Product/ProductInfo.js
@@ -8,11 +8,13 @@ const ProductInfo = props => {
       <span className={styles.productBrand}>{props.product.brand}</span>
       <h3 className={styles.productName}>{props.product.name}</h3>
       <p className={styles.productDesc}>{props.product.description}</p>
-      <div className={styles.productPriceWrapper}>
-        <h5 className={styles.productDiscountedPrice}>${props.product.price * props.product.discount}</h5>
-        <span className={styles.productDiscount}>{props.product.discount * 100}%</span>
-      </div>
-      <del className={styles.productActualPrice}>${props.product.price}</del>
+      <div className={styles.productPriceDesc}>
+        <div className={styles.productPriceWrapper}>
+          <h5 className={styles.productDiscountedPrice}>${props.product.price * props.product.discount}</h5>
+          <span className={styles.productDiscount}>{props.product.discount * 100}%</span>
+        </div>
+        <del className={styles.productActualPrice}>${props.product.price}</del>
+      </div>  
       <AddToCartForm onAddToCart={props.onAddToCart} product={props.product}/>
     </div>
   )
